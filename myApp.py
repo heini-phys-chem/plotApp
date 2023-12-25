d5a3ad35fc81d38727e787c15e2261a7d96ee256
Heinen <stefan.heinen@empa.ch>
2023-12-25 21:23:59 +0100

    removed some comments

--- myApp.py
+++ myApp.py
@@ -7,7 +7,6 @@ from scipy.interpolate import interp1d
 import PySimpleGUI as sg
 
 
-# let's see
 def plot_excel_data(excel_files):
     # Define some line styles
     line_styles = ['-', '--', '-.', ':']
